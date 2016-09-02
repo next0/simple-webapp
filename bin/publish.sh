21dc3e4fee8a407f2c74505f9fcafc823df04594
next0 <next0@yandex-team.ru>
2016-09-03 02:28:10 +0300

    add mocha tests

--- bin/publish.sh
+++ bin/publish.sh
@@ -2,8 +2,11 @@
 set -ev
 
 if [ "$TRAVIS_EVENT_TYPE" == "pull_request" ]; then
-    # docker login
-    docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-    # publish new docker images
-    docker push "$DOCKER_IMAGE_NAME"
+    # check docker registry variables
+    if [[ -n "$DOCKER_EMAIL" && -n "$DOCKER_USERNAME" && -n "$DOCKER_PASSWORD" ]]; then
+        # docker login
+        docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+        # publish new docker images
+        docker push "$DOCKER_IMAGE_NAME"
+    fi
 fi
---
+++ test/index.spec.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+
+describe('index', function () {
+
+    it('self test', function () {
+        expect(true).to.equal(true);
+    });
+
+});
