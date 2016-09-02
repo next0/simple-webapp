7900de366bb6ccd68b8b5a96fdd19fc23ebda29c
next0 <next0@yandex-team.ru>
2016-09-03 02:32:49 +0300

    remove email from docker login command

--- bin/publish.sh
+++ bin/publish.sh
@@ -3,9 +3,9 @@ set -ev
 
 if [ "$TRAVIS_EVENT_TYPE" == "pull_request" ]; then
     # check docker registry variables
-    if [[ -n "$DOCKER_EMAIL" && -n "$DOCKER_USERNAME" && -n "$DOCKER_PASSWORD" ]]; then
+    if [[ -n "$DOCKER_USERNAME" && -n "$DOCKER_PASSWORD" ]]; then
         # docker login
-        docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+        docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
         # publish new docker images
         docker push "$DOCKER_IMAGE_NAME"
     fi
